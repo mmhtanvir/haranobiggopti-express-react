d8f702503cb0c1c6c85e8b6a254d32eedddb1b4d
Tanvir <82567711+mmhtanvir@users.noreply.github.com>
2025-01-17 13:38:59 +0600

    added post cards in home

--- harano-biggopti/client/src/components/home/home.jsx
+++ harano-biggopti/client/src/components/home/home.jsx
@@ -1,12 +1,48 @@
 import React from 'react';
-import { Form, Button, Container, Row, Col } from 'react-bootstrap';
+import { Card, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavigationBar from '../../assets/navigation';
 
 const Home = () => {
   return (
     <>
-    <NavigationBar />
+      <NavigationBar />
+      <div className="container mt-4 text-center">
+        <Row className="justify-content-center">
+          <Col xs={12} md={4}>
+            <Card className="mb-5 shadow-lg">
+              <Card.Img
+                variant="top"
+                src="../images/logo.png"
+                alt="image.jpg"
+                className="rounded-md"
+              />
+              <Card.Body className="text-left">
+                <Card.Title className="font-weight-bold">sdfgxcvgb</Card.Title>
+                <div className="mb-2">
+                  <a href="#" className="font-weight-bold text-decoration-none">
+                    my name
+                  </a>
+                </div>
+                <div className="mb-2">
+                  <a href="/category/person" className="font-weight-bold text-decoration-none">
+                    person
+                  </a>
+                </div>
+                <Card.Text className="text-gray-800">
+                  <strong>Description</strong>: <br /> rfgbfvbh
+                </Card.Text>
+                <Card.Text className="text-muted text-xs">
+                  <strong>2025-01-17 13:15</strong>
+                </Card.Text>
+              </Card.Body>
+                <span className="badge bg-light text-dark py-1 px-3">
+                  #drfgvbtfvtgb
+                </span>
+            </Card>
+          </Col>
+        </Row>
+      </div>
     </>
   );
 };
